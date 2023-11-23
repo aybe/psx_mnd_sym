83bb709764c4b3bceddc4f479ec6da9728aa589a
aybe <aybe@users.noreply.github.com>
2023-11-23 13:42:44 +0100

    Commit formatted cryptic code style.

--- csym/parse_fixups.go
+++ csym/parse_fixups.go
@@ -9,7 +9,9 @@ import (
 
 // RemoveDuplicateTypes goes through parsed types and marks exact duplicates.
 func (p *Parser) RemoveDuplicateTypes() {
-	if p.opts.Verbose { fmt.Printf("Remove duplicate types...\n") }
+	if p.opts.Verbose {
+		fmt.Printf("Remove duplicate types...\n")
+	}
 	p.removeStructsDuplicates()
 	p.removeUnionsDuplicates()
 	p.removeStructsDuplicates() // TODO/BUG: why is this needed?
@@ -43,11 +45,17 @@ func (p *Parser) removeStructsDuplicates() {
 	for _, structs := range p.StructTags {
 		for i := 0; i < len(structs); i++ {
 			t1 := structs[i]
-			if _, ok := typeRemap[t1]; ok { continue }
-			for k := i+1; k < len(structs); k++ {
+			if _, ok := typeRemap[t1]; ok {
+				continue
+			}
+			for k := i + 1; k < len(structs); k++ {
 				t2 := structs[k]
-				if _, ok := typeRemap[t2]; ok { continue }
-				if !reflect.DeepEqual(t2, t1) { continue }
+				if _, ok := typeRemap[t2]; ok {
+					continue
+				}
+				if !reflect.DeepEqual(t2, t1) {
+					continue
+				}
 				typeRemap[t2] = t1
 			}
 		}
@@ -60,7 +68,9 @@ func (p *Parser) removeStructsDuplicates() {
 	}
 	p.ReplaceStructs(typeRemap)
 	p.RmNilStructs()
-	if p.opts.Verbose { fmt.Printf("Removed structs: %d\n", len(typeRemap)) }
+	if p.opts.Verbose {
+		fmt.Printf("Removed structs: %d\n", len(typeRemap))
+	}
 }
 
 // removeUnionsDuplicates goes through parsed symbols and marks exact duplicates.
@@ -70,11 +80,17 @@ func (p *Parser) removeUnionsDuplicates() {
 	for _, unions := range p.UnionTags {
 		for i := 0; i < len(unions); i++ {
 			t1 := unions[i]
-			if _, ok := typeRemap[t1]; ok { continue }
-			for k := i+1; k < len(unions); k++ {
+			if _, ok := typeRemap[t1]; ok {
+				continue
+			}
+			for k := i + 1; k < len(unions); k++ {
 				t2 := unions[k]
-				if _, ok := typeRemap[t2]; ok { continue }
-				if !reflect.DeepEqual(t2, t1) { continue }
+				if _, ok := typeRemap[t2]; ok {
+					continue
+				}
+				if !reflect.DeepEqual(t2, t1) {
+					continue
+				}
 				typeRemap[t2] = t1
 			}
 		}
@@ -87,7 +103,9 @@ func (p *Parser) removeUnionsDuplicates() {
 	}
 	p.ReplaceUnions(typeRemap)
 	p.RmNilUnions()
-	if p.opts.Verbose { fmt.Printf("Removed unions: %d\n", len(typeRemap)) }
+	if p.opts.Verbose {
+		fmt.Printf("Removed unions: %d\n", len(typeRemap))
+	}
 }
 
 // removeEnumsDuplicates goes through parsed symbols and marks exact duplicates.
@@ -97,11 +115,17 @@ func (p *Parser) removeEnumsDuplicates() {
 	for _, enums := range p.EnumTags {
 		for i := 0; i < len(enums); i++ {
 			t1 := enums[i]
-			if _, ok := typeRemap[t1]; ok { continue }
-			for k := i+1; k < len(enums); k++ {
+			if _, ok := typeRemap[t1]; ok {
+				continue
+			}
+			for k := i + 1; k < len(enums); k++ {
 				t2 := enums[k]
-				if _, ok := typeRemap[t2]; ok { continue }
-				if !reflect.DeepEqual(t2, t1) { continue }
+				if _, ok := typeRemap[t2]; ok {
+					continue
+				}
+				if !reflect.DeepEqual(t2, t1) {
+					continue
+				}
 				typeRemap[t2] = t1
 			}
 		}
@@ -114,7 +138,9 @@ func (p *Parser) removeEnumsDuplicates() {
 	}
 	p.ReplaceEnums(typeRemap)
 	p.RmNilEnums()
-	if p.opts.Verbose { fmt.Printf("Removed enums: %d\n", len(typeRemap)) }
+	if p.opts.Verbose {
+		fmt.Printf("Removed enums: %d\n", len(typeRemap))
+	}
 }
 
 // replaceUsedSubtypesInType remaps sub-types within the Type interface.
@@ -220,7 +246,9 @@ func (p *Parser) ReplaceUsedTypes(typeRemap map[c.Type]c.Type) {
 
 // MakeNamesUnique goes through parsed symbols and renames duplicate names.
 func (p *Parser) MakeNamesUnique() {
-	if p.opts.Verbose { fmt.Printf("Making names unique...\n") }
+	if p.opts.Verbose {
+		fmt.Printf("Making names unique...\n")
+	}
 	p.makeStructsUnique()
 	p.makeUnionsUnique()
 	p.makeEnumsUnique()
@@ -241,13 +269,19 @@ func (p *Parser) makeVarNamesUniqueInOverlay(overlay *Overlay) {
 		real_len := 0
 		for i := 0; i < len(variables); i++ {
 			v := variables[i]
-			if v.Class == c.Extern { continue }
+			if v.Class == c.Extern {
+				continue
+			}
 			real_len++
 		}
-		if  real_len < 2 { continue }
+		if real_len < 2 {
+			continue
+		}
 		for i := 0; i < len(variables); i++ {
 			v := variables[i]
-			if v.Class == c.Extern { continue }
+			if v.Class == c.Extern {
+				continue
+			}
 			v.Var.Name = UniqueVarName(overlay.varNames, v)
 		}
 	}
@@ -258,7 +292,9 @@ func (p *Parser) makeFuncNamesUniqueInOverlay(overlay *Overlay) {
 	for _, funcs := range overlay.funcNames {
 		// Do not rename extern declarations
 		real_len := len(funcs)
-		if  real_len < 2 { continue }
+		if real_len < 2 {
+			continue
+		}
 		for i := 0; i < len(funcs); i++ {
 			f := funcs[i]
 			f.Var.Name = UniqueFuncName(overlay.funcNames, f)
@@ -270,7 +306,9 @@ func (p *Parser) makeFuncNamesUniqueInOverlay(overlay *Overlay) {
 func (p *Parser) makeStructsUnique() {
 	for _, structs := range p.StructTags {
 		real_len := len(structs)
-		if  real_len < 2 { continue }
+		if real_len < 2 {
+			continue
+		}
 		for i := 0; i < len(structs); i++ {
 			t := structs[i]
 			t.Tag = UniqueStructTag(p.StructTags, t)
@@ -282,7 +320,9 @@ func (p *Parser) makeStructsUnique() {
 func (p *Parser) makeUnionsUnique() {
 	for _, unions := range p.UnionTags {
 		real_len := len(unions)
-		if  real_len < 2 { continue }
+		if real_len < 2 {
+			continue
+		}
 		for i := 0; i < len(unions); i++ {
 			t := unions[i]
 			t.Tag = UniqueUnionTag(p.UnionTags, t)
@@ -294,7 +334,9 @@ func (p *Parser) makeUnionsUnique() {
 func (p *Parser) makeEnumsUnique() {
 	for _, enums := range p.EnumTags {
 		real_len := len(enums)
-		if  real_len < 2 { continue }
+		if real_len < 2 {
+			continue
+		}
 		for i := 0; i < len(enums); i++ {
 			t := enums[i]
 			t.Tag = UniqueEnumTag(p.EnumTags, t)
@@ -334,9 +376,13 @@ func UniqueStructTag(structTags map[string][]*c.StructType, t *c.StructType) str
 	newTag := t.Tag
 	for {
 		structs, ok := structTags[newTag]
-		if !ok { break } // the tag is unique - done
+		if !ok {
+			break
+		} // the tag is unique - done
 		k := SliceIndex(len(structs), func(i int) bool { return structs[i] == t })
-		if k < 0 { k = len(structs) }
+		if k < 0 {
+			k = len(structs)
+		}
 		newTag = UniqueTag(newTag, "s", k)
 	}
 	return newTag
@@ -348,9 +394,13 @@ func UniqueUnionTag(unionTags map[string][]*c.UnionType, t *c.UnionType) string 
 	newTag := t.Tag
 	for {
 		unions, ok := unionTags[newTag]
-		if !ok { break } // the tag is unique - done
+		if !ok {
+			break
+		} // the tag is unique - done
 		k := SliceIndex(len(unions), func(i int) bool { return unions[i] == t })
-		if k < 0 { k = len(unions) }
+		if k < 0 {
+			k = len(unions)
+		}
 		newTag = UniqueTag(newTag, "u", k)
 	}
 	return newTag
@@ -362,11 +412,14 @@ func UniqueEnumTag(EnumTags map[string][]*c.EnumType, t *c.EnumType) string {
 	newTag := t.Tag
 	for {
 		enums, ok := EnumTags[newTag]
-		if !ok { break } // the tag is unique - done
+		if !ok {
+			break
+		} // the tag is unique - done
 		k := SliceIndex(len(enums), func(i int) bool { return enums[i] == t })
-		if k < 0 { k = len(enums) }
+		if k < 0 {
+			k = len(enums)
+		}
 		newTag = UniqueTag(newTag, "e", k)
 	}
 	return newTag
 }
-
